7881ce4ce11862492908adedb82339376634aae1
esraa-kaf <esraakaf3@gmail.com>
2024-02-21 09:47:01 +0200

    add file

--- models/plantsAvatars.js
+++ models/plantsAvatars.js
@@ -1,19 +1,23 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 const AutoIncrement = require("mongoose-sequence")(mongoose)
-const plantsAvatarSchema=new mongoose.Schema({
-    _id:{
-        type:Number
-     },
-  avatar:String,
-  status:{
-    type: String,
-     enum: ["after", "before"],
-     default:"before"
+const plantsAvatarSchema = new mongoose.Schema({
+    _id: {
+        type: Number
     },
-    plants_Id:ref('plantsModel')
+    avatar: String,
+    status: {
+        type: String,
+        enum: ["after", "before"],
+        default: "before"
+    },
+    plants_Id: {
+        type: Number,
+        ref: "plantsModel",
+        required: true
+    }
 
-  
+
 })
 plantsAvatarSchema.plugin(AutoIncrement, { id: 'plantsAvatarCounter' });
-const plantsAvatars = mongoose.model('plantsAvatars',plantsAvatarSchema)
-module.exports=plantsAvatars
+const plantsAvatars = mongoose.model('plantsAvatars', plantsAvatarSchema)
+module.exports = plantsAvatars
