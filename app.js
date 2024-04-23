d3e2e6451648e3e68aa262f1e64d8a1178d5caee
esraa-kaf <esraakaf3@gmail.com>
2024-04-24 01:50:34 +0200

    `vercel config`

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ app.use([userRouter,engineerRouter,plantsRouter])
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 
-mongoose.connect('mongodb+srv://esraakaf3:<IZj9EVHTPX7i7EUB>@esraakaf.vu1anyf.mongodb.net/')
+mongoose.connect('mongodb+srv://esraakaf3:IZj9EVHTPX7i7EUB@esraakaf.vu1anyf.mongodb.net/')
 .then(()=>{
 
     const PORT =process.env.PORT ||3000;
